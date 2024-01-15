e6bc0cdf9aee3b5328f4118e487411cc2ae57792
granth2023 <grantharris38@gmail.com>
2024-01-15 00:00:02 -0500

    syntax for width css fixed

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ import dash_html_components as html
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    html.H1("River")
+app.layout = html.Div ([
+    html.H1("River"),
     html.Video(
         src="url.m3u8",
-        controls = True,
-        autoplay= True,
-        style= { "width"= "100%"}
-    )
-    
+        controls=True,
+        autoPlay=True,
+        style= { "width": "100%"}
+    )  
 ])
